47b6f30b7e590248fd0ac9815dc454e32a315772
AbdullahRehmat <rehmat.dev@gmail.com>
2022-01-19 20:29:38 +0300

    Initial Commit
    Added all project file to GitHub Repository. Inital commit to Repo.

---
+++ MainWindow.py
@@ -0,0 +1,140 @@
+import sys
+import time
+
+from PyQt6.QtWidgets import QApplication, QMainWindow
+from UI_MainWindow import Ui_mainWindow
+
+
+class MainWindow:
+    def __init__(self):
+        self.main_win = QMainWindow()
+        self.ui = Ui_mainWindow()
+        self.ui.setupUi(self.main_win)
+
+        # Define UI Elements
+        self.lcd = self.ui.lcd_display
+        self.time_edit = self.ui.time_edit_box
+        self.status_bar = self.ui.statusbar
+
+        # Set Timer Loop Status
+        self.timer_status = False
+
+        # Set GUI Init Value
+        self.display_contents = "0:00"
+
+        # Connect UI Button to Respective Functions
+        self.ui.start_timer_btn.clicked.connect(self.start_timer)
+        self.ui.reset_timer_btn.clicked.connect(self.reset_timer)
+
+    def show(self):
+        self.main_win.show()
+
+    def timer(self, duration):
+        seconds = 59
+        duration -= 1
+        seconds, duration = int(seconds), int(duration)
+
+        while self.timer_status == True:
+
+            if duration == 0 and seconds == 0:
+                # Write to Display
+                self.display_contents = "-:--"
+                self.update_lcd()
+
+                # Flash Status Bar Message
+                self.status_bar.showMessage("Timer Ended")
+
+                # End Function
+                self.timer_status = False
+
+            elif duration == 0 and seconds > 0:
+                # Format Time
+                if seconds <= 9:
+                    self.display_contents = str(duration) + ":0" + str(seconds)
+                else:
+                    self.display_contents = str(duration) + ":" + str(seconds)
+
+                # Write to Display
+                self.update_lcd()
+
+                # Time Logic
+                seconds -= 1
+                time.sleep(1)
+
+            elif duration > 0 and seconds == 0:
+                # Format Time
+                if seconds <= 9:
+                    self.display_contents = str(duration) + ":0" + str(seconds)
+                else:
+                    self.display_contents = str(duration) + ":" + str(seconds)
+
+                # Write to Display
+                self.update_lcd()
+
+                # Time Logic
+                duration -= 1
+                seconds += 59
+                time.sleep(1)
+
+            elif duration > 0 and seconds > 0:
+                # Format Time
+                if seconds <= 9:
+                    self.display_contents = str(duration) + ":0" + str(seconds)
+                else:
+                    self.display_contents = str(duration) + ":" + str(seconds)
+
+                # Write to Display
+                self.update_lcd()
+
+                # Time Logic
+                seconds -= 1
+                time.sleep(1)
+
+            else:
+                # Flash Status Bar Message
+                self.status_bar.showMessage("Error!")
+
+                # End Function + Exit App
+                self.timer_status = False
+                time.sleep(1)
+                sys.exit(1)
+
+    def start_timer(self):
+        # Flash Status Bar Message
+        self.status_bar.showMessage("Timer Started")
+
+        # Get Time Value From Input Box
+        time_input = self.time_edit.time().minute()
+
+        # Check if a time if provided
+        if time_input <= 0:
+            # Flash Status Bar Message
+            self.status_bar.showMessage("Time <= 0 Minutes")
+
+        # Start Timer
+        self.timer_status = True
+
+        # Call Timer Function
+        self.timer(time_input)
+
+    def reset_timer(self):
+        # Set Timer Loop Status
+        self.timer_status = False
+
+        # Clear LCD Screen
+        self.display_contents = "0"
+        self.update_lcd()
+
+        # Flash Status Bar Message
+        self.status_bar.showMessage("Timer Reset")
+
+    def update_lcd(self):
+        self.lcd.display(self.display_contents)
+        self.lcd.repaint()
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    main_win = MainWindow()
+    main_win.show()
+    sys.exit(app.exec())
---
+++ README.md
@@ -0,0 +1,55 @@
+# Positron - Timer
+
+A basic timer written in Python with the PyQt6 framework.  
+Forms part of the Positron Software Suite.  
+
+
+
+## Structure
+
+### Source Code Files
+
+- `MainWindow.py` - Main Application File
+
+- `UI_MainWindow.py` - Qt-Designer File Converted to Python
+
+- `UI_MainWindow.ui` - QT-Designer File
+
+  
+
+### Python Package Files
+
+- `Pipfile`
+- `Pipfile.lock`
+- `requirements.txt`
+
+
+
+### PyInstaller Files
+
+- `/build`
+- `/dist` 
+- `MainWindow.spec`
+
+
+
+## Build Instructions
+
+### To Modify Project
+
+- If required open `UI_MainWindow.ui` file in Qt-Designer and modify layout
+- Run `pyuic6 -x UI_MainWindow.ui -o UI_MainWindow.py` to convert `.ui` to Python
+
+
+
+### To Debug
+
+- Run `python3 MainWindow.py` to launch application to debug
+
+
+
+### To Build
+
+- Run `pipenv run python3 freeze` and save output to `requirements.txt`
+- Run `pyinstaller --onefile MainWindow.py` to compile project to single executable file
+
